37d6e9b3612ec6cfc43ef149a7fcfcd7d76fed70
aibolscorpion <AI57451298ai>
2020-11-26 16:38:21 +0600

    first

---
+++ app/src/main/java/com/example/test/InformationAboutUserActivity.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InformationAboutUserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_about_user);
+
+    }
+}
